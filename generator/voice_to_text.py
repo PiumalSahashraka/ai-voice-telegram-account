485fe8c5615cac3fa1c9fcdb0667179771084a96
PiumalSahashraka <piumalsahashraka@gmail.com>
2024-08-15 12:29:39 +0530

    file structure changes

--- generator/voice_to_text.py
+++ generator/voice_to_text.py
@@ -1,15 +1,11 @@
-from deepgram import (
-    DeepgramClient,
-    PrerecordedOptions,
-    FileSource,
-)
+from deepgram import DeepgramClient,PrerecordedOptions,FileSource
 import json
+
 from config import Config
 
 deepgram = DeepgramClient(Config.DG_API_KEY)
 
 def translate(audio_url: str):
-
     with open(audio_url, "rb") as file:
             buffer_data = file.read()
 
@@ -21,15 +17,14 @@ def translate(audio_url: str):
         model="nova-2",
         smart_format=True,
     )
-
     response = deepgram.listen.prerecorded.v("1").transcribe_file(payload, options)
-    result = response.to_json(indent=4)
-    result = json.loads(result)
+    results = json.loads(response.to_json(indent=4))
     try:
-        text_list = result['results']['channels'][0]['alternatives'][0]['words']
-        text = " ".join([word['word'] for word in text_list])
+        text_list = results['results']['channels'][0]['alternatives'][0]['words']
+        return " ".join([word['word'] for word in text_list])
     except KeyError:
-         return "Sorry, I couldn't understand that."
+        return "Sorry, I couldn't understand that."
     except TypeError:
-         return "Sorry, I couldn't understand that."
-    return text
+        return "Sorry, I couldn't understand that."
+    except Exception:
+        return "Sorry, I couldn't understand that."
